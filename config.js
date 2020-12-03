e14070b178435096fc1d834f61b65faae284d11c
test51 <test51@getkor.io>
2020-12-03 14:33:19 -0600

    First commit

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+  cognito: {
+      userPoolId: 'us-east-1_Tn8rHyeTk',
+      clientId: '17qcvvng76t6pfp18pprpnmgts',
+      region: 'us-east-1'
+  },
+  api: {
+      invokeUrl: 'https://9kdl1yecjj.execute-api.us-east-1.amazonaws.com/v1/template',
+  }
+};
